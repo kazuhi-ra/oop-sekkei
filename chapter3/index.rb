ade3b33d6b404c1d5daa76ce6d530912cd5d3b06
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-12 03:42:23 +0900

    Hash#fetchを使って引数がbooleanの場合にも対応

--- chapter3/index.rb
+++ chapter3/index.rb
@@ -2,8 +2,8 @@ class Gear
   attr_reader :chainring, :cog, :wheel
 
   def initialize(args)
-    @chainring = args[:chainring] || 40
-    @cog = args[:cog] || 18
+    @chainring = args.fetch(:chainring, 40)
+    @cog = args.fetch(:cog, 18)
     @wheel = args[:wheel]
   end
 
@@ -42,7 +42,7 @@ class Wheel
 end
 
 p Gear.new({
-  chainring: 52,
+  # chainring: 52,
   cog: 11,
   wheel: Wheel.new(26, 1.5),
 }).gear_inches
