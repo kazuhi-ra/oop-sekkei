5ce65d411c8387e1282257d9b1e4fe6bf1b37048
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-12 04:19:55 +0900

    依存の逆転の前の準備

--- chapter3/index.rb
+++ chapter3/index.rb
@@ -1,38 +1,18 @@
-module SomeFramework
-  class Gear
-    attr_reader :chainring, :cog, :wheel
+class Gear
+  attr_reader :chainring, :cog, :wheel
 
-    def initialize(chainring, cog, wheel)
-      @chainring = chainring
-      @cog = cog
-      @wheel = wheel
-    end
-
-    def ratio
-      chainring / cog.to_f
-    end
-
-    def gear_inches
-      ratio * diameter
-    end
-
-    def diameter
-      wheel.diameter
-    end
-
-    def wheel
-      @wheel || Wheel.new(rim, tire)
-    end
+  def initialize(chainring, cog, wheel=nil)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
   end
-end
 
-module GearWrapper
-  def self.gear(args)
-    SomeFramework::Gear.new(
-      args[:chainring],
-      args[:cog],
-      args[:wheel],
-    )
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
   end
 end
 
@@ -53,8 +33,5 @@ class Wheel
   end
 end
 
-p GearWrapper.gear({
-  chainring: 28,
-  cog: 11,
-  wheel: Wheel.new(26, 1.5),
-}).gear_inches
+@wheel = Wheel.new(26, 1.5)
+p Gear.new(52, 11, @wheel).gear_inches
