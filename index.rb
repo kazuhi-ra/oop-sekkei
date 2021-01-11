6423caa70ec96e3457edd7adbbdd5211e12417f1
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-11 22:23:40 +0900

    RubyのStructクラスを使って構造を包み隠す

--- index.rb
+++ index.rb
@@ -33,3 +33,25 @@ class ObscuringReferences
 end
 
 p ObscuringReferences.new([[10, 100], [50, 500]]).diameters
+
+class RevealingReferences
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect do |wheel|
+      wheel.rim + wheel.tire * 2
+    end
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect do |cell|
+      Wheel.new(cell[0], cell[1])
+    end
+  end
+end
+
+p RevealingReferences.new([[10, 100], [50, 500]]).diameters
