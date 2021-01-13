1065dd85b7d22e0f1cb8135f3cf598f15145878c
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-13 17:43:36 +0900

    依存爆発

--- chapter5/index.rb
+++ chapter5/index.rb
@@ -1,11 +1,21 @@
 class Trip
-  attr_reader :bicycles
-  def initialize(bicycles, mechanic)
+  attr_reader :bicycles, :customers, :vehicle
+  def initialize(bicycles)
     @bicycles = bicycles
   end
 
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(preparers)
+    preparers.each do |preparer|
+      case prepare
+      when Mechanic
+        prepare.prepare_bicycles(bicycles)
+      when TripCoordinator
+        prepare.buy_food(customers)
+      when Driver
+        prepare.gas_up(vehicle)
+        prepare.fill_water_tank(vehicle)
+      end
+    end
   end
 end
 
@@ -20,6 +30,20 @@ def prepare_bicycles(bicycles)
     puts "#{bicycle}を整備しました"
   end
 end
+
+class TripCoordinator
+  def prepare_bicycles(bicycles)
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+  end
+  
+  def fill_water_tank(vehicle)
+  end
+end
+
 
 mechanic = Mechanic.new()
 trip = Trip.new(['kaz', 'hit', 'pas'], mechanic)
