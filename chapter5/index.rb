378772180b877abd710ce99c0ff90c9a4b26de59
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-13 18:12:36 +0900

    ダックタイピングへ置き換え

--- chapter5/index.rb
+++ chapter5/index.rb
@@ -1,49 +1,51 @@
 class Trip
   attr_reader :bicycles, :customers, :vehicle
-  def initialize(bicycles)
+  def initialize(bicycles, customers, vehicle)
     @bicycles = bicycles
+    @customers = customers
+    @vehicle = vehicle
   end
 
   def prepare(preparers)
-    preparers.each do |preparer|
-      case prepare
-      when Mechanic
-        prepare.prepare_bicycles(bicycles)
-      when TripCoordinator
-        prepare.buy_food(customers)
-      when Driver
-        prepare.gas_up(vehicle)
-        prepare.fill_water_tank(vehicle)
-      end
-    end
+    preparers.each {|preparer|
+      prepare.prepare_trip(self)}
   end
 end
 
 class Mechanic
-def prepare_bicycles(bicycles)
-    bicycles.each do |bicycle|
+  def prepare_trip(trip)
+    trip.bicycles.each do |bicycle|
       prepare_bicycle(bicycle)
     end
   end
 
   def prepare_bicycle(bicycle)
-    puts "#{bicycle}を整備しました"
+    puts "#{bicycle}を準備しました"
   end
 end
 
 class TripCoordinator
-  def prepare_bicycles(bicycles)
+  def prepare_trip(trip)
+    buy_foods(trip.customers)
+  end
+
+  def buy_foods(customers)
+    puts "#{customers.size}人分のご飯を用意しました"
   end
 end
 
 class Driver
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+
   def gas_up(vehicle)
+    puts "#{vehicle}のガソリンを満タンにしました"
   end
   
   def fill_water_tank(vehicle)
+    puts "#{vehicle}のウォーターサーバーを満タンにしました"
   end
 end
-
-
-mechanic = Mechanic.new()
-trip = Trip.new(['kaz', 'hit', 'pas'], mechanic)
