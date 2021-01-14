19792a02da9c59c04cbacd909866598387ca1d2a
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-14 23:13:33 +0900

    sizeを親クラスに移動

--- chapter6/index.rb
+++ chapter6/index.rb
@@ -1,16 +1,19 @@
 class Bicycle
+  attr_reader :size
 
+  def initialize(args={})
+    @size = args[:size]
+  end
 end
 
-class RoadBike
-  attr_reader :style, :size, :tape_color,
-            :front_shock, :rear_shock
+class RoadBike < Bicycle
+  attr_reader :style, :tape_color, :front_shock, :rear_shock
   def initialize(args)
     @style = args[:style]
-    @size = args[:size]
     @tape_color = args[:tape_color]
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
+    super(args)
   end
 
   def spares
@@ -46,13 +49,12 @@ end
 
 p RoadBike.new(
   size: 'S',
-  front_shock: 'Manitou',
-  rear_shock: 'Fox',
-).spares
+  tape_color: 'red',
+).size
 
 
 p MountainBike.new(
   size: 'M',
   front_shock: 'Manitou',
   rear_shock: 'Fox',
-).spares
+).size
