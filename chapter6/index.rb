8bbdc599d992694f929c2319fb07a55e450e0df4
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-14 23:41:36 +0900

    superの部分に強い依存がある

--- chapter6/index.rb
+++ chapter6/index.rb
@@ -14,15 +14,20 @@ class Bicycle
   def default_tire_size
     raise NotImplementedError
   end
+
+  def spares
+    {
+      chain: chain,
+      tire_size: tire_size,
+    }
+  end
 end
 
 class RoadBike < Bicycle
-  attr_reader :style, :tape_color, :front_shock, :rear_shock
+  attr_reader :tape_color
+
   def initialize(args)
-    @style = args[:style]
     @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
     super(args)
   end
 
@@ -31,19 +36,7 @@ class RoadBike < Bicycle
   end
 
   def spares
-    if style == :road
-      {
-        chain: '10-speed',
-        tire_size: '23',
-        tape_color: tape_color,
-      }
-    else
-      {
-        chain: '10-speed',
-        tire_size: '2.1',
-        rear_shock: rear_shock,
-      }
-    end
+    super.merge(tape_color: tape_color)
   end
 end
 
@@ -68,11 +61,11 @@ end
 p RoadBike.new(
   size: 'S',
   tape_color: 'red',
-).tire_size
+).spares
 
 
 p MountainBike.new(
   size: 'M',
   front_shock: 'Manitou',
   rear_shock: 'Fox',
-).tire_size
+).spares
