5a6a9f24843778631660ea14af9f84907de56d3b
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-14 23:26:46 +0900

    chainとtire_sizeを昇格

--- chapter6/index.rb
+++ chapter6/index.rb
@@ -1,8 +1,14 @@
 class Bicycle
-  attr_reader :size
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args={})
     @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end
+
+  def default_chain
+    '10-speed'
   end
 end
 
@@ -14,6 +20,10 @@ class RoadBike < Bicycle
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
     super(args)
+  end
+
+  def default_tire_size
+    '23'
   end
 
   def spares
@@ -42,6 +52,10 @@ class MountainBike < Bicycle
     super(args)
   end
 
+  def default_tire_size
+    '2.1'
+  end
+
   def spares
     super.merge(rear_shock: rear_shock)
   end
@@ -50,11 +64,11 @@ end
 p RoadBike.new(
   size: 'S',
   tape_color: 'red',
-).size
+).tire_size
 
 
 p MountainBike.new(
   size: 'M',
   front_shock: 'Manitou',
   rear_shock: 'Fox',
-).size
+).tire_size
