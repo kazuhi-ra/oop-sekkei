459d54b4a31491277fa15c33e36d767f0404a7d7
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-22 15:58:38 +0900

    sparesを使ってみる

--- chapter8/index.rb
+++ chapter8/index.rb
@@ -12,7 +12,7 @@ class Bicycle
 end
 
 class Parts
-  attr_reader :chain, :tire_size
+  attr_reader :parts
 
   def initialize(parts)
     @parts = parts
@@ -33,49 +33,14 @@ class Part
   end
 end
 
-class RoadBikeParts < Parts
-  attr_reader :tape_color
+chain = Part.new(name: "chain", description: "10-speed")
+road_tire = Part.new(name: "tire_size", description: "23")
+tape = Part.new(name: "tape", description: "red")
 
-  def post_initialize(args)
-    @tape_color = args[:tape_color]
-  end
-
-  def local_spares
-    { tape_color: tape_color }
-  end
-
-  def default_tire_size
-    "23"
-  end
-end
-
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
-
-  def post_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
-
-  def default_tire_size
-    "2.1"
-  end
-
-  def local_spares
-    { rear_shock: rear_shock }
-  end
-end
-
-rbp = RoadBikeParts.new(
-  tape_color: "red",
-)
-mbp = MountainBikeParts.new(
-  front_shock: nil,
-  rear_shock: nil,
-)
+road_bike_parts = Parts.new([chain, road_tire, tape])
 
 road_bike = Bicycle.new(
   size: "23",
-  parts: rbp,
+  parts: road_bike_parts,
 )
-p road_bike.spares
+puts road_bike.spares
